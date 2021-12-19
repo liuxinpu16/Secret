3132a8dba12d89fb8d3f065e2433f5a374b9fe55
liuxinpu16 <liuxinpu16@gmail.com>
2021-12-18 19:02:49 -0600

    encrypt with env var

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/userDB');
@@ -11,11 +13,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email:String,
   password:String
-};
+});
 
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']})
 const User = mongoose.model('User',userSchema);
 
 app.get('/',function(req,res){
@@ -46,13 +50,13 @@ app.post('/register',function(req,res){
 
 app.post('/login',function(req,res){
   const username = req.body.username;
-  const userpassw = req.body.password
-  User.find({email:req.body.username}, function(err,founduser){
+  const userpassw = req.body.password;
+  User.findOne({email:username}, function(err,founduser){
     if(err){
       console.log(err);
     }else{
       if(founduser){
-        if(userpassw == founduser.password){
+        if(userpassw === founduser.password){
           res.render('secrets')
         }else{
           console.log('wrong password');
